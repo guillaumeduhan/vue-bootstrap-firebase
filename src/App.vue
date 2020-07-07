885406d63791854652f73019957651c9e644e84a
Guillaume Duhan <myhappyagency@gmail.com>
2020-07-07 22:45:27 +0200

    Prettier on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
-	<div id="app">
-		<div class="container">
-			<h1>My-first-vue-project</h1>
-		</div>
-	</div>
+  <div id="app">
+    <div class="container">
+      <h1>My-first-vue-project</h1>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	name: "App"
+  name: "App",
 };
 </script>
 
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
